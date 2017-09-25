a7f3873908d30f10fd6c6f4a7ada4c1b494270e3
libaqu7890 <libaqu7890@gmail.com>
2017-09-25 09:38:37 +0900

    리덕스 주석 제거

--- client/src/Home/HomeCute/index.js
+++ client/src/Home/HomeCute/index.js
@@ -116,35 +116,3 @@ export default connect(
   })
 
 )(HomeCute);
-
-
-// class HomeCute extends Component {
-
-//   handleSubmit = (e) => {
-//     e.preventDefault();
-//     const { cuteValue } = this.props;
-//     axios.post('/api/cute', 
-//       {
-//         cuteValue: cuteValue
-//       })
-//   }
-//   handleChange = (e) => {
-//     const { homeActions } = this.props
-//     const cuteValue = e.target.value
-//     homeActions.change({cuteValue})
-//   }
-
-//   render() {
-//     const {handleChange, handleSubmit} = this
-//     const {cuteValue} = this.props
-
-//     return (
-//       <div>
-//         <form onSubmit={handleSubmit}>
-//           <input type="text" name="contents" value={cuteValue} onChange={handleChange} /><button type="submit">submit</button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
